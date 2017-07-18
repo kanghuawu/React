b4958ad76027b53cf0f1ff1be7f0e1374ef852e9
kanghuawu <kang-hua.wu@sjsu.edu>
2017-07-17 22:15:11 -0700

    add hoc

---
+++ hoc/src/components/resource.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <li>1 Cup Sugar</li>
+      <li>1 Cup Salt</li>
+      <li>1 Cup Pepper</li>
+    </div>
+  );
+}
